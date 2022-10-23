aaaa0059df309130aede8921fd27ca803db958a1
Akib Ali <akib7599@gmail.com>
2022-10-23 14:18:28 +0530

    temporary delete

--- src/component/googleui.jsx
+++ src/component/googleui.jsx
@@ -4,16 +4,22 @@ export const GoogleUI=(props)=>{
 
     const [strike, setstrike] = useState(false)
 
+
+    const temphandledelete=()=>{
+        setstrike(true)
+
+    }
+
     return (
         
             <tbody>
-                <tr>
+                <tr style={{ textDecorationColor:"red",textDecoration: strike ? "line-through" : "none"}}>
                     <td>{props.id}</td>
                     <td>{props.elem.title_note}</td>
                     <td>{props.elem.date}</td>
                     <td>{props.elem.decription}</td>
                     <td>{props.elem.rating}</td>
-                    <td><button>Temprarory Delete</button></td>
+                    <td><button onClick={temphandledelete}>Temprarory Delete</button></td>
                     <td><button onClick={(()=> props.handleDelete(props.index))}>Delete Note</button></td>
                 </tr>
             </tbody>
